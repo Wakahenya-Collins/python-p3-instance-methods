7d66d19ece71a9b8db180445fbbb0c2f412ee9aa
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-08-25 00:30:03 +0300

    Done with assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,22 @@
-#!/usr/bin/env python3
+# from dog import Dog
+# from person import Person
+
 
 class Person:
-    # Class body goes here
+    # def __init__(self, name):
+    #     self.name = name
+    
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+
+
+
+
+
+
+
+
---
+++ lib/testing/test.py
@@ -0,0 +1,11 @@
+from dog import Dog
+from person import Person
+
+dog = Dog("Buddy")
+person = Person("Alice")
+
+dog.bark()
+dog.sit()
+
+person.talk()
+person.walk()
